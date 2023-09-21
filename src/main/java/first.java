6f03d30975eb7a642a2aaa88b30155b04f05a72b
Hameza <gamezo2021@inbox.ru>
2023-09-21 19:28:39 +0300

    Initial commit

---
+++ src/main/java/first.java
@@ -0,0 +1,10 @@
+public class first {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i;
+        for(i = 0; i < 10; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Сумма" + sum);
+    }
+}
